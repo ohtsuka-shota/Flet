2a0c76a8a1cfc7226931a58185e13cb4169c1994
ohtsuka-shota <ohtsuka.honban@gmail.com>
2024-05-12 17:55:10 +0900

    test

--- first-app/main.py
+++ first-app/main.py
@@ -3,10 +3,11 @@ import flet as ft
 
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+
     input_example = ft.TextField(
-        label="test",
+        label="input_example",
     )
 
-    return ft.Column(controls=[input_example])
+    page.add(input_example)
 
 ft.app(main)
